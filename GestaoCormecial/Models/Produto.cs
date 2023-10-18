efe20dde9b3aecc2396da3b4aced8486297ce5e1
Guilherme Brito Moura <148218733+guilhermebm99@users.noreply.github.com>
2023-10-18 11:07:43 -0300

    Criação das 3 primeiras classes

---
+++ GestaoCormecial/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public double Preco { get; set; }
+
+        public double Estoque { get; set; }
+
+
+    }
+}
