7636b543d8030155bd1fe53659f452983252cb24
Guilherme Brito Moura <148218733+guilhermebm99@users.noreply.github.com>
2023-10-18 11:34:11 -0300

    mudança pois estava incorreto
    errei o nome dos objetos da clliente dall e não coloquei  classe produtos  dall , agora está correto

---
+++ GestaoCormecial/DAL/Produto.cs
@@ -0,0 +1,49 @@
+﻿
+
+namespace DAL
+{
+    public class Produto
+    {
+
+        public void Inserir(Produto _produto)
+        {
+
+
+
+        }
+        public void Alterar(Produto _produto)
+        {
+
+
+
+
+        }
+        public void Excluir(Produto _produto)
+        {
+
+
+
+
+        }
+        public List<Produto> BuscarTodos()
+        {
+
+            throw new NotImplementedException();
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
