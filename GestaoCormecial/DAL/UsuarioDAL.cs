1471eb24f2d0e6e6cd539ba833da356dec0d7a2d
Guilherme Brito Moura <148218733+guilhermebm99@users.noreply.github.com>
2023-10-19 11:30:56 -0300

    Inicio da implementação do método Inserir  na UsuarioDAL
    Dia 20/10/2023 ESSA IMPLEMENTAÇÃO DEVE SER CONCLUÍDA

--- GestaoCormecial/DAL/UsuarioDAL.cs
+++ GestaoCormecial/DAL/UsuarioDAL.cs
@@ -1,6 +1,7 @@
 ﻿using Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,18 @@ namespace DAL
     public class UsuarioDAL
     {
         public void Inserir(Usuario _usuario)
-        { 
-        
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection();   
+                SqlCommand cmd = cn.CreateCommand();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Ocorreu um erro ao tentar inserir o usuário no banco de dados",ex);
+            }
         
         
         }
